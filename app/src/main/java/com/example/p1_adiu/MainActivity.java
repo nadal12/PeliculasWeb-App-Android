8638579323343a23f7a67dc61d37ba9cf7b8b6b0
Carlos <carlosboocrugeiras@gmail.com>
2021-01-13 21:15:13 +0100

    Realiza consultas web correctamente y la librería de gráficos ya está importada

--- app/src/main/java/com/example/p1_adiu/MainActivity.java
+++ app/src/main/java/com/example/p1_adiu/MainActivity.java
@@ -2,74 +2,94 @@ package com.example.p1_adiu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-import com.android.volley.toolbox.Volley;
-import com.android.volley.RequestQueue;
+
+import com.anychart.anychart.AnyChart;
+import com.anychart.anychart.AnyChartView;
+import com.anychart.anychart.DataEntry;
+import com.anychart.anychart.Pie;
+import com.anychart.anychart.ValueDataEntry;
+
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import static com.android.volley.toolbox.Volley.newRequestQueue;
+
 
 public class MainActivity extends AppCompatActivity {
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mContext = getApplicationContext();
         setContentView(R.layout.activity_main);
 
         View mainButton = (Button) findViewById(R.id.button);
         mainButton.setOnClickListener(v -> {
             getData();
+            consulta();
+            System.out.println("Final");
         });
+
+
     }
 
     private void getData() {
         Toast.makeText(MainActivity.this, "Botón pulsado", Toast.LENGTH_LONG).show();
 
     }
+
+
+
+
     /**
+     * CODIGO PARA REALIZAR LA CONSULTA
+     */
 
-    private static final String TAG = "MiActivity";
+    // link para pruebas: https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json
 
-//...
-    JsonObjectRequest jsObjectRequest;
+    String sURL="https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json";
 
-    String sURL="http://www.example.com";
-    Queue requestQueue= Volley.newRequestQueue(this);
+    public void consulta(){
+        // Nueva petición JSONObject
+        RequestQueue requestQueue = Volley.newRequestQueue(mContext);
 
-// Nueva petición JSONObject
-    jsObjectRequest =
-            new JsonObjectRequest(
-            DownloadManager.Request.Method.GET, sURL,"",
-            new Response.Listener<JSONObject>() {
-        @Override
-        public void onResponse(JSONObject response) {
-            procesarRespuesta(response);
-        }
-    },
-            new Response.ErrorListener() {
-        @Override
-        public void onErrorResponse(VolleyError error) {
-            VolleyErrorHelper errorVolley = new VolleyErrorHelper();
-            String sError = VolleyErrorHelper.getMessage(error, getApplicationContext());
-            Log.d(TAG, "Error: " + sError);
-        }
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, sURL, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        procesarRespuesta(response);
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        System.out.println("Error");
+                    }
+                });
+
+        requestQueue.add(jsonObjectRequest);
+
     }
-);
 
-// Añadir petición a la cola
-requestQueue.add(jsObjectRequest);
-
-//...
-
-//Método para procesar la respuesta (parsear el JSON)
+    //Método para procesar la respuesta (parsear el JSON)
 
     protected String procesarRespuesta(JSONObject jsObject) {
         //Procesar el JSON
-
+        System.out.println("JSON: " + jsObject.toString());
+        return "";
     }
-     **/
 }
